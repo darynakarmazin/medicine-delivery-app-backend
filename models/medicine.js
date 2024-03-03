e38f8cf1f0cb5824ea861f7ecfb8820a6a9d6855
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-04 00:31:02 +0200

    Updated model for medicines

--- models/medicine.js
+++ models/medicine.js
@@ -5,7 +5,7 @@ const medicineSchema = Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    price: { type: Date, required: true },
+    price: { type: String, required: true },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "shop",
