0dc7330d1045f95ed31479234e3e64146e1705ff
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-04 16:10:25 +0200

    Created schema for medicine orders.

--- models/order.js
+++ models/order.js
@@ -1,8 +1,21 @@
 const { Schema, model } = require("mongoose");
 
+const medicineSchema = Schema({
+  _id: { type: String, required: true },
+  title: { type: String, required: true },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "shop",
+  },
+});
+
 const orderSchema = Schema(
   {
     name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    medicines: [medicineSchema],
   },
   { versionKey: false, timestamps: true }
 );
